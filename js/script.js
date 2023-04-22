2faf199c80942d7394f11352ed6765c03fadde37
taki-takitsubo <129972070+taki-takitsubo@users.noreply.github.com>
2023-04-22 13:51:03 +0900

    launch

--- js/script.js
+++ js/script.js
@@ -6,8 +6,18 @@
      let rect=fadeIn.getBoundingClientRect().top;
      let scroll=window.pageYOffset;
      let offset=rect+scroll;
-     if(scroll>offset-windowHeight+150){
+     if(scroll>offset-windowHeight+160){
        fadeIn.classList.add('fadein-active');
      };
    });
  });
+
+ // デバイス識別
+ if (navigator.userAgent.indexOf('iPhone') > 0 || navigator.userAgent.indexOf('Android') > 0 && navigator.userAgent.indexOf('Mobile') > 0) {
+} else if (navigator.userAgent.indexOf('iPad') > 0 || navigator.userAgent.indexOf('Android') > 0) {
+    // タブレット向けの記述
+} else {
+    luxy.init({
+      wrapperSpeed: 0.08
+    });
+}
